d4635b469d74c7946c4f36000dabbd5ddf4690e5
Albanlv <albanlv@gmail.com>
2012-07-27 11:14:29 +0200

    variables for serious website dev

--- app.rb
+++ app.rb
@@ -44,4 +44,37 @@ post '/killer_contract' do
   erb :killer_contract
 end
 
+post '/website_development' do
+  @developer_name = params[:developer_name]
+  puts @developer_name
+  @developer_status = params[:developer_status]
+  puts @developer_status
+  @company_name = params[:company_name]
+  puts @company_name
+  @company_business = params[:company_business]
+  puts @company_business
+  @notice_delay = params[:notice_delay]
+  puts @notice_delay
+  @cure_delay = params[:cure_delay]
+  puts @cure_delay
+  @copyright_delay = params[:copyright_delay]
+  puts @copyright_delay
+  @maintenance_period = params[:maintenance_period]
+  puts @maintenance_period
+  @warranty_period = params[:warranty_period]
+  puts @warranty_period
+  @delay = params[:delay]
+  puts @delay
+  @first_payment = params[:first_payment]
+  puts @first_payment
+  @second_payment = params[:second_payment]
+  puts @second_payment
+  @third_payment = params[:third_payment]
+  puts @third_payment
+  @price_per_hour = params[:price_per_hour]
+  puts @price_per_hour
+  
+  erb :website_development
+end
+
 $LOAD_PATH.unshift(Dir.getwd)
